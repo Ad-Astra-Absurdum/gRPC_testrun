8118c3a8909aa228375195a259ee2aa2b55df456
Gary <35435241+GaryDeco@users.noreply.github.com>
2023-03-03 22:35:09 -0500

    Dumping initial setup
    Quickly assembled starting point

---
+++ Plugin.cs
@@ -0,0 +1,14 @@
+﻿using BepInEx;
+
+namespace kRPC2_gRPC_test
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public void Awake()
+        {
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+            //TODO
+        }
+    }
+}
